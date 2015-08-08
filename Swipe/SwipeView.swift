933047c0a2541929fe2eb418583261fb732729c0
Brett Wallace <bwallace90@gmail.com>
2015-08-08 16:05:44 -0400

    Set up SwipeView class and constraints

---
+++ Swipe/SwipeView.swift
@@ -0,0 +1,45 @@
+//
+//  SwipeView.swift
+//  Swipe
+//
+//  Created by Brett Wallace on 8/8/15.
+//  Copyright (c) 2015 Brett Wallace. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SwipeView: UIView {
+    private let card: CardView = CardView()
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialize()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+    
+    convenience init() {
+        self.init()
+        initialize()
+    }
+    
+    private func initialize() {
+        self.backgroundColor = UIColor.clearColor()
+        addSubview(card)
+        
+        card.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        setConstraints()
+    }
+    
+    private func setConstraints() {
+        addConstraint(NSLayoutConstraint(item: card, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 0))
+        addConstraint(NSLayoutConstraint(item: card, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 0))
+        addConstraint(NSLayoutConstraint(item: card, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 0))
+        addConstraint(NSLayoutConstraint(item: card, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: 0))
+    }
+}
