a948fb5373c4e4028ccc27e9f1c5d850f54ae800
Brett Wallace <bwallace90@gmail.com>
2015-08-12 20:59:21 -0400

    Set up logic to save user details to Parse

--- Swipe/LoginViewController.swift
+++ Swipe/LoginViewController.swift
@@ -27,6 +27,23 @@ class LoginViewController: UIViewController {
                 println("The user canceled the Facebook login")
             } else if user!.isNew {
                 println("User signed up and logged in thru Facebook")
+                
+                FBRequestConnection.startWithGraphPath("/me?fields=picture, first_name, birthday, gender", completionHandler: {
+                    connection, result, error in
+                        var r = result as! NSDictionary
+                        user!["firstName"] = r["first_name"]
+                        user!["gender"] = r["gender"]
+                        user!["picture"] = ((r["picture"] as! NSDictionary)["data"] as! NSDictionary)["url"]
+                        var dateFormatter = NSDateFormatter()
+                        dateFormatter.dateFormat = "MM/dd/yyyy"
+                        user!["birthday"] = dateFormatter.dateFromString(r["birthday"] as! String)
+                    
+                    user!.saveInBackgroundWithBlock({
+                        success, error in
+                            println(success)
+                            println(error)
+                    })
+                })
             } else {
                 println("User logged in thru Facebook")
             }
