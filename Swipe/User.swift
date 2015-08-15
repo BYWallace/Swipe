1efd4aae3ce20d6b6af8e3e916df691d00e0fbe9
Brett Wallace <bwallace90@gmail.com>
2015-08-15 15:56:41 -0400

    Set up ProfileViewController to update with image file from Parse

--- Swipe/User.swift
+++ Swipe/User.swift
@@ -10,19 +10,26 @@ import Foundation
 
 struct User {
     let id: String
-    let pictureURL: String
     let name: String
     private let pfUser: PFUser
+    
+    func getPhoto(callback:(UIImage) -> ()) {
+        let imageFile = pfUser.objectForKey("picture") as! PFFile
+        imageFile.getDataInBackgroundWithBlock { (data, error) -> Void in
+            if let data = data {
+                callback(UIImage(data: data)!)
+            }
+        }
+    }
 }
 
 private func pfUserToUser(user: PFUser) -> User {
-    return User(id: user.objectId!, pictureURL: user.objectForKey("picture") as! String, name: user.objectForKey("firstName") as! String, pfUser: user)
+    return User(id: user.objectId!, name: user.objectForKey("firstName") as! String, pfUser: user)
 }
 
 func currentUser() -> User? {
     if let user = PFUser.currentUser() {
         return pfUserToUser(user)
     }
-    
     return nil
 }
