eddf1d9eb9b35c21b5713e84b4f4e8d388d785e6
Brett Wallace <bwallace90@gmail.com>
2015-08-15 15:42:05 -0400

    Fix bug when creating new user due to typo in FBGraph request URL

--- Swipe/LoginViewController.swift
+++ Swipe/LoginViewController.swift
@@ -30,21 +30,25 @@ class LoginViewController: UIViewController {
             } else if user!.isNew {
                 println("User signed up and logged in thru Facebook")
                 
-                FBRequestConnection.startWithGraphPath("/me?fields=picture, first_name, birthday, gender", completionHandler: {
+                FBRequestConnection.startWithGraphPath("/me?fields=picture,first_name,birthday,gender", completionHandler: {
                     connection, result, error in
                         var r = result as! NSDictionary
                         user!["firstName"] = r["first_name"]
                         user!["gender"] = r["gender"]
-                        user!["picture"] = ((r["picture"] as! NSDictionary)["data"] as! NSDictionary)["url"]
                         var dateFormatter = NSDateFormatter()
                         dateFormatter.dateFormat = "MM/dd/yyyy"
                         user!["birthday"] = dateFormatter.dateFromString(r["birthday"] as! String)
                     
-                    user!.saveInBackgroundWithBlock({
-                        success, error in
-                            println(success)
-                            println(error)
+                    let pictureURL = ((r["picture"] as! NSDictionary)["data"] as! NSDictionary)["url"] as! String
+                    let url = NSURL(string: pictureURL)
+                    let request = NSURLRequest(URL: url!)
+                    
+                    NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: { (response, data, error) -> Void in
+                        let imageFile = PFFile(name: "avatar.jpg", data: data)
+                        user!["picture"] = imageFile
+                        user!.saveInBackgroundWithBlock(nil)
                     })
+
                 })
             } else {
                 println("User logged in thru Facebook")
