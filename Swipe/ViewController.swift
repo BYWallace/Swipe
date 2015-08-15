aa12e10da8f1cc5fa03f13b4ade6d80200eba3c7
Brett Wallace <bwallace90@gmail.com>
2015-08-15 16:18:26 -0400

    Implement PageViewController as main VC

--- Swipe/ViewController.swift
+++ Swipe/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIPageViewController, UIPageViewControllerDataSource {
+    let cardsVC: UIViewController! = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("CardsNavController") as! UIViewController
+    let profileVC: UIViewController! = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("ProfileNavController") as! UIViewController
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.whiteColor()
+        self.dataSource = self
+        
+        self.setViewControllers([cardsVC], direction: UIPageViewControllerNavigationDirection.Forward, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - UIPageViewControllerDataSource
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        switch viewController {
+        case cardsVC:
+            return profileVC
+        case profileVC:
+            return nil
+        default:
+            return nil
+        }
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        switch viewController {
+        case cardsVC:
+            return nil
+        case profileVC:
+            return cardsVC
+        default:
+            return nil
+        }
+    }
 
 }
 
