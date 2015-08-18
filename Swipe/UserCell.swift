ad6911f6fadc3067a13b224c1ef378e265f30b5d
Brett Wallace <bwallace90@gmail.com>
2015-08-17 21:50:22 -0400

    Set up NavigationTableView for matches and UserCell

---
+++ Swipe/UserCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCell.swift
+//  Swipe
+//
+//  Created by Brett Wallace on 8/17/15.
+//  Copyright (c) 2015 Brett Wallace. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
