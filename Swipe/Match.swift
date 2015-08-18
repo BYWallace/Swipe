472755bf763260aa8e9b0693149f5bc72d74ac0c
Brett Wallace <bwallace90@gmail.com>
2015-08-17 22:27:05 -0400

    Set up fetchMatches function and implement in viewWillAppear

---
+++ Swipe/Match.swift
@@ -0,0 +1,45 @@
+//
+//  Match.swift
+//  Swipe
+//
+//  Created by Brett Wallace on 8/17/15.
+//  Copyright (c) 2015 Brett Wallace. All rights reserved.
+//
+
+import Foundation
+
+struct Match {
+    let id: String
+    let user: User
+}
+
+func fetchMatches (callback: ([Match]) -> ()) {
+    PFQuery(className: "Action")
+        .whereKey("byUser", equalTo: PFUser.currentUser()!.objectId!)
+        .whereKey("type", equalTo: "matched")
+        .findObjectsInBackgroundWithBlock({
+            objects, error in
+            if let matches = objects as? [PFObject] {
+                let matchedUsers = matches.map({
+                    (object)->(matchID: String, userID: String) in
+                    (object.objectId!, object.objectForKey("toUser") as! String)
+                })
+                let userIDs = matchedUsers.map({$0.userID})
+                
+                PFUser.query()?
+                    .whereKey("objectId", containedIn: userIDs)
+                    .findObjectsInBackgroundWithBlock({
+                        objects, error in
+                        
+                        if let users = objects as? [PFUser] {
+                            var users = reverse(users)
+                            var m = Array<Match>()
+                            for(index, user) in enumerate(users) {
+                                m.append(Match(id: matchedUsers[index].matchID, user: pfUserToUser(user)))
+                            }
+                            callback(m)
+                        }
+                    })
+            }
+        })
+}
